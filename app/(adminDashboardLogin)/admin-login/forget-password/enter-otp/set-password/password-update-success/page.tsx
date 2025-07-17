884549e18475c1f6614bfb6a710d9e17bb3442c4
Hossain <hossainahamedk@gmail.com>
2025-07-17 17:19:26 +0600

    added go to login page

--- app/(adminDashboardLogin)/admin-login/forget-password/enter-otp/set-password/password-update-success/page.tsx
+++ app/(adminDashboardLogin)/admin-login/forget-password/enter-otp/set-password/password-update-success/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import mainLogo from '@/public/images/main-logo.png';
+import Link from "next/link";
 
 export default function PasswordUpdateSuccessPage() {
     return (
@@ -16,6 +17,13 @@ export default function PasswordUpdateSuccessPage() {
                 <h1 className="text-3xl font-semibold text-center">Password Updated Successfully!</h1>
                 <p className="text-lg text-center">Your new password has been saved. You can now continue securely.</p>
             </div>
+
+            <Link href="/admin-login" className="mt-8">
+                <button className="bg-[#4B2A99] text-white px-6 py-2 rounded-xl text-lg font-semibold shadow hover:bg-[#3a217a] transition">
+                    Go to Login
+                </button>
+            </Link>
+
         </div>
     )
 }
